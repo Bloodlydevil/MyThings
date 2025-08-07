a9e1d8ecc5ad1b433a9032a5c2f739642849e506
unknown <aryandev8396@gmail.com>
2025-08-07 18:53:29 +0530

    AudioText

---
+++ Scripts/Data/AudioText.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace MyThings.Data
+{
+
+    [Serializable]
+    public class AudioText
+    {
+        public string Text;
+        public AudioClip Audio;
+    }
+}
